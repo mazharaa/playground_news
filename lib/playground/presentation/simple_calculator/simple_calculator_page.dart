09fc3b08a1427a8a246e20197985b544162e5b05
mazharaa <mazharaa32@gmail.com>
2023-11-01 00:31:49 +0700

    Add Reusabel Widget
    Add reusable operation button widget for Simple Calculator UI

--- lib/playground/presentation/simple_calculator/simple_calculator_page.dart
+++ lib/playground/presentation/simple_calculator/simple_calculator_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:playground_news/core/commons/color_const.dart';
 import 'package:playground_news/core/utils/text_theme_extension.dart';
 import 'package:playground_news/core/utils/ui_helper.dart';
+import 'package:playground_news/playground/presentation/simple_calculator/widgets/operation_button.dart';
 
 @RoutePage()
 class SimpleCalculatorPage extends StatelessWidget {
@@ -52,32 +53,6 @@ class SimpleCalculatorPage extends StatelessWidget {
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class OperationButton extends StatelessWidget {
-  const OperationButton({
-    super.key,
-    required this.operation,
-  });
-
-  final String operation;
-
-  @override
-  Widget build(BuildContext context) {
-    return OutlinedButton(
-      style: OutlinedButton.styleFrom(
-        minimumSize: Size(
-          UiHelper.setSp(73),
-          UiHelper.setSp(38),
-        ),
-      ),
-      onPressed: () {},
-      child: Text(
-        operation,
-        style: context.textTheme.bodyMedium,
       ),
     );
   }
